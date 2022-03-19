a7483f6236b7574815706bea0cd77844d1e6b285
vutrongduong <duongvu.050898@gmail.com>
2022-03-19 15:25:37 +0700

    demo

--- furama_web_service/src/main/java/vn/codegym/model/customer/CustomerType.java
+++ furama_web_service/src/main/java/vn/codegym/model/customer/CustomerType.java
@@ -42,6 +42,6 @@ public class CustomerType {
 
     public void setCustomerList(List<Customer> customerList) {
         this.customerList = customerList;
-        "okelao kia";
+        "okelao kia khong  hconge";
     }
 }
